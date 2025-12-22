20262f213c8ea8e8c0634dfd0a0222c69a2151a2
Lorenzo Rossi <mail@lorenzoros.si>
2025-12-22 22:07:59 +0100

    Added empty telegram tests

---
+++ tests/test_telegram.py
@@ -0,0 +1,18 @@
+"""This module contains unit tests for the telegram module."""
+
+import unittest
+
+from corgos_telegram_bot.modules.telegram import Telegram
+
+
+class TestTelegramModule(unittest.IsolatedAsyncioTestCase):
+    """Unit tests for the Telegram module."""
+
+    async def asyncSetUp(self) -> None:
+        """Set up the Telegram instance for testing."""
+        self.settings_path = "tests/data/telegram_unit_test_settings.json"
+
+    async def testInitializeTelegram(self) -> None:
+        """Test initializing the Telegram instance."""
+        telegram = Telegram(settings_path=self.settings_path)
+        await telegram.start()
